22932a4b51589538204c41577a23ccfa72936455
孙新宇 <602184489@qq.com>
2019-12-20 17:19:01 +0800

    修改pdfjs文件夹下build为pdfbuild，避免不能提交

--- showPdf/pdfJs/web/viewer.js
+++ showPdf/pdfJs/web/viewer.js
@@ -4667,7 +4667,7 @@
 				kind: OptionKind.WORKER
 			},
 			workerSrc: {
-				value: '../build/pdf.worker.js',
+				value: '../pdfBuild/pdf.worker.js',
 				kind: OptionKind.WORKER
 			}
 		}; {
@@ -4775,7 +4775,7 @@
 		if(typeof window !== 'undefined' && window['pdfjs-dist/build/pdf']) {
 			pdfjsLib = window['pdfjs-dist/build/pdf'];
 		} else {
-			pdfjsLib = require('../build/pdf.js');
+			pdfjsLib = require('../pdfBuild/pdf.js');
 		}
 
 		module.exports = pdfjsLib;
